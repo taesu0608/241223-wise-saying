20a105d96ddb6e0e89fbf05c516e4913d1bafdf0
taesu0608 <taesu0608@gmail.com>
2024-12-23 17:13:26 +0900

    work5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,22 +14,15 @@ class App {
 
         Scanner s = new Scanner(System.in);
 
-        System.out.print("명언 : ");
-        s.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
-        System.out.print("작가 : ");
-        s.nextLine();
-        System.out.println("1번 명언이 등록되었습니다.");
-        System.out.println("명령) 종료");
         while (true) {
             System.out.print("명령) ");
             String command = s.nextLine();
-            if (command.equals("등록")) {
+            if(command.equals("종료")) {
+                System.out.print("명언 앱을 종료합니다.");
+                break;
+            } else if (command.equals("등록")){
                 System.out.print("명언 : ");
-                s.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
-                System.out.print("작가 : ");
                 s.nextLine();
-
-                System.out.println("1번 명언이 등록되었습니다.");
             }
         }
     }
