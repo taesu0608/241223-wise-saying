91f03d122fb81a52714bf24690da927fa62c0e64
taesu0608 <taesu0608@gmail.com>
2024-12-23 15:20:14 +0900

    work3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
        App app = new App();
@@ -9,7 +11,15 @@ public class Main {
 
 class App{
     public void run(){
+
+        Scanner s = new Scanner(System.in);
+
         System.out.println("--명언 앱--");
+        System.out.print("명언 : ");
+        s.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다.
+        System.out.print("작가 : ");
+        s.nextLine();
+        System.out.println("1번 명언이 등록되었습니다.");
         System.out.println("명령) 종료");
-    }
+        }
 }
