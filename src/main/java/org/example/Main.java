165f7b0d1614d517d5535261e8000fdcff539046
taesu0608 <taesu0608@gmail.com>
2024-12-23 15:09:30 +0900

    work2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-       System.out.println("-- 명언 앱 --");
-       System.out.println("명령) 종료");
+       App app = new App();
+
+    }
+}
+
+class App{
+    public void run(){
+        System.out.println("--명언 앱--");
+        System.out.println("명령) 종료");
     }
 }
