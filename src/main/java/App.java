3ed7993d777e4d795daf3615c4aa02fb9115d86e
taesu0608 <taesu0608@gmail.com>
2024-12-24 12:33:35 +0900

    work19

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,10 @@
-package org.example;
-
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        App app = new App();
-        app.run();
-    }
-}
-
-class App {
-
+public class App {
     // 클래스에서 함수들이 공유해야 하는 변수 -> 인스턴스 변수
     private int lastId = 0;
     private int wiseSayingSize = 0;
-    private WiseSaying[] wiseSayingList = new WiseSaying[3];
+    private final WiseSaying[] wiseSayingList = new WiseSaying[3];
 
     public void run() {
 
@@ -38,7 +28,7 @@ class App {
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
 
-                add(content, author); // 1. 함수로 분리 -> 코드가 줄어든다. 가독성이 올라간다. 2. 재활용성(중복 제거)
+                add(content, author);
 
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
             } else if(command.equals("목록")) {
@@ -57,31 +47,6 @@ class App {
     public void add(String content, String author) {
         WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
         wiseSayingList[wiseSayingSize++] = wiseSaying;
-    }
-
-}
-
-class WiseSaying {
-    private int id;
-    private String content;
-    private String author;
-
-    WiseSaying(int id, String content, String author) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getAuthor() {
-        return author;
     }
 
 }
