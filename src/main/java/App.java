5aae19d58f977d01509a2ffe3564a425f0664678
taesu0608 <taesu0608@gmail.com>
2024-12-24 12:42:38 +0900

    work20

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,10 +5,10 @@ public class App {
     private int lastId = 0;
     private int wiseSayingSize = 0;
     private final WiseSaying[] wiseSayingList = new WiseSaying[3];
+    Scanner scanner = new Scanner(System.in);
 
     public void run() {
 
-        Scanner scanner = new Scanner(System.in);
         add("꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "월트 디즈니");
 
         System.out.println("== 명언 앱 ==");
@@ -19,27 +19,10 @@ public class App {
             if(command.equals("종료")) {
                 System.out.println("명언 앱을 종료합니다.");
                 break;
-
             } else if(command.equals("등록")) {
-
-                System.out.print("명언 : ");
-                String content = scanner.nextLine();
-
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
-
-                add(content, author);
-
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+                writeWiseSaying();
             } else if(command.equals("목록")) {
-
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("----------------------");
-
-                for(int i = 0; i < wiseSayingSize; i++) {
-                    WiseSaying wiseSaying = wiseSayingList[i];
-                    System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
-                }
+                printWiseSayingList();
             }
         }
     }
@@ -49,4 +32,23 @@ public class App {
         wiseSayingList[wiseSayingSize++] = wiseSaying;
     }
 
+    public void printWiseSayingList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+
+        for(int i = 0; i < wiseSayingSize; i++) {
+            WiseSaying wiseSaying = wiseSayingList[i];
+            System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
+        }
+    }
+    public void writeWiseSaying() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+
+        add(content, author);
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+    }
 }
